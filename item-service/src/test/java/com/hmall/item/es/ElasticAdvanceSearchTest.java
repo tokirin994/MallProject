dd146513e77e43d2fd37a136b7bdcff9a65f7831
tokirin994 <12046037532@qq.com>
2025-01-07 16:45:14 +0800

    Function: [Elasticsearch] basic usage of es (just in test files)

---
+++ item-service/src/test/java/com/hmall/item/es/ElasticAdvanceSearchTest.java
@@ -0,0 +1,204 @@
+package com.hmall.item.es;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.SortOrder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.hmall.common.utils.CollUtils;
+import com.hmall.item.domain.po.ItemDoc;
+import com.hmall.item.service.IItemService;
+
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest(properties = "spring.profiles.active=local")
+public class ElasticAdvanceSearchTest {
+
+    private RestHighLevelClient client;
+    @Autowired
+    private IItemService itemService;
+
+    @Test
+    void testConnection() {
+        System.out.println("client = " + client);
+    }
+
+    @BeforeEach
+    void setup() {
+        client = new RestHighLevelClient(RestClient.builder(
+                HttpHost.create("http://192.168.163.130:9200")));
+    }
+
+    @Test
+    void testMatchAll() throws IOException {
+        // 1.创建Request
+        SearchRequest request = new SearchRequest("items");
+        // 2.组织请求参数
+        request.source().query(QueryBuilders.matchAllQuery());
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+    private void handleResponse(SearchResponse response) {
+        SearchHits searchHits = response.getHits();
+        // 1.获取总条数
+        long total = searchHits.getTotalHits().value;
+        System.out.println("共搜索到" + total + "条数据");
+        // 2.遍历结果数组
+        SearchHit[] hits = searchHits.getHits();
+        for (SearchHit hit : hits) {
+            // 3.得到_source，也就是原始json文档
+            String source = hit.getSourceAsString();
+            // 4.反序列化并打印
+            ItemDoc item = JSONUtil.toBean(source, ItemDoc.class);
+            System.out.println(item);
+        }
+    }
+
+    @Test
+    void testBool() throws IOException {
+        // 1.创建Request
+        SearchRequest request = new SearchRequest("items");
+        // 2.组织请求参数
+        // 2.1.准备bool查询
+        BoolQueryBuilder bool = QueryBuilders.boolQuery();
+        // 2.2.关键字搜索
+        bool.must(QueryBuilders.matchQuery("name", "脱脂牛奶"));
+        // 2.3.品牌过滤
+        bool.filter(QueryBuilders.termQuery("brand", "德亚"));
+        // 2.4.价格过滤
+        bool.filter(QueryBuilders.rangeQuery("price").lte(30000));
+        request.source().query(bool);
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+    @Test
+    void testPageAndSort() throws IOException {
+        int pageNo = 1, pageSize = 5;
+
+        // 1.创建Request
+        SearchRequest request = new SearchRequest("items");
+        // 2.组织请求参数
+        // 2.1.搜索条件参数
+        request.source().query(QueryBuilders.matchQuery("name", "脱脂牛奶"));
+        // 2.2.排序参数
+        request.source().sort("price", SortOrder.ASC);
+        // 2.3.分页参数
+        request.source().from((pageNo - 1) * pageSize).size(pageSize);
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+    @Test
+    void testHighlight() throws IOException {
+        // 1.创建Request
+        SearchRequest request = new SearchRequest("items");
+        // 2.组织请求参数
+        // 2.1.query条件
+        request.source().query(QueryBuilders.matchQuery("name", "脱脂牛奶"));
+        // 2.2.高亮条件
+        request.source().highlighter(
+                SearchSourceBuilder.highlight()
+                        .field("name")
+                        .preTags("<em>")
+                        .postTags("</em>"));
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponseHighlight(response);
+    }
+
+    private void handleResponseHighlight(SearchResponse response) {
+        SearchHits searchHits = response.getHits();
+        // 1.获取总条数
+        long total = searchHits.getTotalHits().value;
+        System.out.println("共搜索到" + total + "条数据");
+        // 2.遍历结果数组
+        SearchHit[] hits = searchHits.getHits();
+        for (SearchHit hit : hits) {
+            // 3.得到_source，也就是原始json文档
+            String source = hit.getSourceAsString();
+            // 4.反序列化
+            ItemDoc item = JSONUtil.toBean(source, ItemDoc.class);
+            // 5.获取高亮结果
+            Map<String, HighlightField> hfs = hit.getHighlightFields();
+            if (CollUtils.isNotEmpty(hfs)) {
+                // 5.1.有高亮结果，获取name的高亮结果
+                HighlightField hf = hfs.get("name");
+                if (hf != null) {
+                    // 5.2.获取第一个高亮结果片段，就是商品名称的高亮值
+                    String hfName = hf.getFragments()[0].string();
+                    item.setName(hfName);
+                }
+            }
+            System.out.println(item);
+        }
+    }
+
+    @Test
+    void testAgg() throws IOException {
+        // 1.创建Request
+        SearchRequest request = new SearchRequest("items");
+        // 2.准备请求参数
+        BoolQueryBuilder bool = QueryBuilders.boolQuery()
+                .filter(QueryBuilders.termQuery("category", "手机"))
+                .filter(QueryBuilders.rangeQuery("price").gte(300000));
+        request.source().query(bool).size(0);
+        // 3.聚合参数
+        request.source().aggregation(
+                AggregationBuilders.terms("brand_agg").field("brand").size(5));
+        // 4.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 5.解析聚合结果
+        Aggregations aggregations = response.getAggregations();
+        // 5.1.获取品牌聚合
+        Terms brandTerms = aggregations.get("brand_agg");
+        // 5.2.获取聚合中的桶
+        List<? extends Terms.Bucket> buckets = brandTerms.getBuckets();
+        // 5.3.遍历桶内数据
+        for (Terms.Bucket bucket : buckets) {
+            // 5.4.获取桶内key
+            String brand = bucket.getKeyAsString();
+            System.out.print("brand = " + brand);
+            long count = bucket.getDocCount();
+            System.out.println("; count = " + count);
+        }
+    }
+
+    @AfterEach
+    void teardown() throws IOException {
+        if (client != null) {
+            client.close();
+        }
+    }
+}
---
+++ item-service/src/test/java/com/hmall/item/es/ElasticTest.java
@@ -0,0 +1,215 @@
+package com.hmall.item.es;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmall.common.utils.CollUtils;
+import com.hmall.item.domain.po.Item;
+import com.hmall.item.domain.po.ItemDoc;
+import com.hmall.item.service.IItemService;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest(properties = "spring.profiles.active=local")
+public class ElasticTest {
+
+    private RestHighLevelClient client;
+    @Autowired
+    private IItemService itemService;
+
+    @Test
+    void testConnection() {
+        System.out.println("client = " + client);
+    }
+
+    @BeforeEach
+    void setup() {
+        client = new RestHighLevelClient(RestClient.builder(
+                HttpHost.create("http://192.168.163.130:9200")));
+    }
+
+    @Test
+    void testCreateIndex() throws IOException {
+        // 1.创建Request对象
+        CreateIndexRequest request = new CreateIndexRequest("items");
+        // 2.准备请求参数
+        request.source(MAPPING_TEMPLATE, XContentType.JSON);
+        // 3.发送请求
+        client.indices().create(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testDeleteIndex() throws IOException {
+        // 1.创建Request对象
+        DeleteIndexRequest request = new DeleteIndexRequest("items");
+        // 2.发送请求
+        client.indices().delete(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testExistsIndex() throws IOException {
+        // 1.创建Request对象
+        GetIndexRequest request = new GetIndexRequest("items");
+        // 2.发送请求
+        boolean exists = client.indices().exists(request, RequestOptions.DEFAULT);
+        // 3.输出
+        System.err.println(exists ? "索引库已经存在！" : "索引库不存在！");
+    }
+
+    // *********************************************Document*************************************
+    @Test
+    void testAddDocument() throws IOException {
+        // 1.根据id查询商品数据
+        Item item = itemService.getById(100002644680L);
+        // 2.转换为文档类型
+        ItemDoc itemDoc = BeanUtil.copyProperties(item, ItemDoc.class);
+        // 3.将ItemDTO转json
+        String doc = JSONUtil.toJsonStr(itemDoc);
+
+        // 1.准备Request对象
+        IndexRequest request = new IndexRequest("items").id(itemDoc.getId());
+        // 2.准备Json文档
+        request.source(doc, XContentType.JSON);
+        // 3.发送请求
+        client.index(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testGetDocumentById() throws IOException {
+        // 1.准备Request对象
+        GetRequest request = new GetRequest("items").id("100002644680");
+        // 2.发送请求
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        // 3.获取响应结果中的source
+        String json = response.getSourceAsString();
+
+        ItemDoc itemDoc = JSONUtil.toBean(json, ItemDoc.class);
+        System.out.println("itemDoc= " + itemDoc);
+    }
+
+    @Test
+    void testDeleteDocument() throws IOException {
+        // 1.准备Request，两个参数，第一个是索引库名，第二个是文档id
+        DeleteRequest request = new DeleteRequest("item", "100002644680");
+        // 2.发送请求
+        client.delete(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testUpdateDocument() throws IOException {
+        // 1.准备Request
+        UpdateRequest request = new UpdateRequest("items", "100002644680");
+        // 2.准备请求参数
+        request.doc(
+                "price", 58800,
+                "commentCount", 1);
+        // 3.发送请求
+        client.update(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testLoadItemDocs() throws IOException {
+        // 分页查询商品数据
+        int pageNo = 1;
+        int size = 1000;
+        while (true) {
+            Page<Item> page = itemService.lambdaQuery().eq(Item::getStatus, 1).page(new Page<Item>(pageNo, size));
+            // 非空校验
+            List<Item> items = page.getRecords();
+            if (CollUtils.isEmpty(items)) {
+                return;
+            }
+            log.info("加载第{}页数据，共{}条", pageNo, items.size());
+            // 1.创建Request
+            BulkRequest request = new BulkRequest("items");
+            // 2.准备参数，添加多个新增的Request
+            for (Item item : items) {
+                // 2.1.转换为文档类型ItemDTO
+                ItemDoc itemDoc = BeanUtil.copyProperties(item, ItemDoc.class);
+                // 2.2.创建新增文档的Request对象
+                request.add(new IndexRequest()
+                        .id(itemDoc.getId())
+                        .source(JSONUtil.toJsonStr(itemDoc), XContentType.JSON));
+            }
+            // 3.发送请求
+            client.bulk(request, RequestOptions.DEFAULT);
+
+            // 翻页
+            pageNo++;
+        }
+    }
+
+    @AfterEach
+    void teardown() throws IOException {
+        if (client != null) {
+            client.close();
+        }
+    }
+
+    static final String MAPPING_TEMPLATE = "{\n" +
+            "  \"mappings\": {\n" +
+            "    \"properties\": {\n" +
+            "      \"id\": {\n" +
+            "        \"type\": \"keyword\"\n" +
+            "      },\n" +
+            "      \"name\":{\n" +
+            "        \"type\": \"text\",\n" +
+            "        \"analyzer\": \"ik_max_word\"\n" +
+            "      },\n" +
+            "      \"price\":{\n" +
+            "        \"type\": \"integer\"\n" +
+            "      },\n" +
+            "      \"stock\":{\n" +
+            "        \"type\": \"integer\"\n" +
+            "      },\n" +
+            "      \"image\":{\n" +
+            "        \"type\": \"keyword\",\n" +
+            "        \"index\": false\n" +
+            "      },\n" +
+            "      \"category\":{\n" +
+            "        \"type\": \"keyword\"\n" +
+            "      },\n" +
+            "      \"brand\":{\n" +
+            "        \"type\": \"keyword\"\n" +
+            "      },\n" +
+            "      \"sold\":{\n" +
+            "        \"type\": \"integer\"\n" +
+            "      },\n" +
+            "      \"commentCount\":{\n" +
+            "        \"type\": \"integer\"\n" +
+            "      },\n" +
+            "      \"isAD\":{\n" +
+            "        \"type\": \"boolean\"\n" +
+            "      },\n" +
+            "      \"updateTime\":{\n" +
+            "        \"type\": \"date\"\n" +
+            "      }\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
+
+}
